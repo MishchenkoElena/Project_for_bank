5941fbf148a40313031e13307b2bc1aed097ea06
Елена Мищенко <mitschenkoelena8@gmail.com>
2024-12-09 23:44:20 +0300

    add main.py

--- src/widget.py
+++ src/widget.py
@@ -1,10 +1,13 @@
+from src.masks import get_mask_account, get_mask_card_number
+
+
 def mask_account_card(user_accounting: str) -> str:
     """Маскировка номера банковской карты или счета"""
 
-    if "MasterCard" or "Maestro" or "Visa" in user_accounting:
-        return f" {user_accounting[0:-12]} {user_accounting[-12:-10]}** **** {user_accounting[-4:]}"
+    if "Счет" in user_accounting:
+        return f" Счет {get_mask_account(user_accounting)}"
     else:
-        return f" Счет **{user_accounting[-4:]}"
+        return f" {user_accounting[:-16]} {get_mask_card_number(user_accounting[-16:])}"
 
 
 def get_date(user_login_date: str) -> str:
