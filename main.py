19a4a3a851894e902a76544822f4ab8f51d7cd19
Елена Мищенко <mitschenkoelena8@gmail.com>
2024-12-30 19:16:10 +0300

    tests added

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-from src.processing import filter_by_state
-from src.processing import sort_by_date
+from src.processing import filter_by_state, sort_by_date
+from src.widget import get_date, mask_account_card
+
+print(mask_account_card("Visa 6831982476737658"))
+print(mask_account_card("Master Card 6831982476737658"))
+print(mask_account_card("счет 73654108430135874305"))
+
+print(get_date("2024-03-11T02:26:18.671407"))
 
 print(
     filter_by_state(
